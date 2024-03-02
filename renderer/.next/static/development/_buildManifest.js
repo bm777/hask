18e689df043dd4411061002c3d8d300b1ccbc5b0
clappo143 <95749676+clappo143@users.noreply.github.com>
2024-03-03 00:07:18 +1100

    3March

---
+++ renderer/.next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/hask":["static\u002Fchunks\u002Fpages\u002Fhask.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fhask"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()

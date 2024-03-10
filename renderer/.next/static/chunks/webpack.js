3a482aca6bd65e3675dcc7136cf5d04220e62d50
clappo143 <95749676+clappo143@users.noreply.github.com>
2024-03-11 00:30:41 +1100

    Add capitalize function and ThemeProvider*** ***Update globals.css and _app.js*** ***Add new constants and get_lang function*** ***Update provider.js*** ***Remove unused code in background.js

--- renderer/.next/static/chunks/webpack.js
+++ renderer/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "108923b65cdc81a5"; }
+/******/ 		__webpack_require__.h = function() { return "6c351bf488a31536"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

37c313fb7bd05418817b53e7c26df37ed78e709f
clappo143 <95749676+clappo143@users.noreply.github.com>
2024-04-10 00:08:18 +1000

    Main changes: Stylisation tweaks - removed text from copy buttons, now just clipboard icon.
    Seems to all be working ok, but not properly
    tested
    
    Other changes: fixes to URL parsing

--- renderer/components/parsedText.js
+++ renderer/components/parsedText.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const ParsedText = ({ text }) => {
-    return (
-        <div
-            className="markdown-body text-sm"
-            dangerouslySetInnerHTML={{ __html: text }}
-        />
-    );
+  return (
+    <div
+      className="markdown-body text-sm"
+      dangerouslySetInnerHTML={{ __html: text }}
+    />
+  );
 };
 
 export default ParsedText;
